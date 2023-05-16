d704c27f7baf34d7c92f9c32851b54970f6531b2
shivam907 <shivamkaushal907@gmail.com>
2023-05-16 15:18:29 +0530

    Fixed the Number Animation

--- src/Components/Numbers/Numbers.jsx
+++ src/Components/Numbers/Numbers.jsx
@@ -1,59 +1,18 @@
 import React from "react";
 import classes from "./Numbers.module.css";
 
-let timer;
+import CountUp from "react-countup";
 function Numbers() {
-  // const App = () => {
-  const [counter, setCounter] = React.useState(0);
-  React.useEffect(() => {
-    const interval = setInterval(() => {
-      if (counter < 1839) {
-        setCounter(counter + 1);
-      }
-    }, 1);
-
-    return () => {
-      clearInterval(interval);
-    };
-  });
-  const [counter1, setCounter1] = React.useState(0);
-  React.useEffect(() => {
-    const interval = setInterval(() => {
-      if (counter1 < 512) {
-        setCounter1(counter1 + 1);
-      }
-    }, 1.2);
-
-    return () => {
-      clearInterval(interval);
-    };
-  });
-  const [counter2, setCounter2] = React.useState(0);
-  React.useEffect(() => {
-    const interval = setInterval(() => {
-      if (counter2 < 729) {
-        setCounter2(counter2 + 1);
-      }
-    }, 1.2);
-
-    return () => {
-      clearInterval(interval);
-    };
-  });
-  const [counter3, setCounter3] = React.useState(0);
-  React.useEffect(() => {
-    const interval = setInterval(() => {
-      if (counter3 < 2729) {
-        setCounter3(counter3 + 1);
-      }
-    }, 0.75);
-
-    return () => {
-      clearInterval(interval);
-    };
-  });
-  // return <div>{counterState}</div>;
-  // };
+  const [loading, setLoading] = React.useState(false);
+  const onStart = () => {
+    setLoading(true);
+  };
+  const onEnd = () => {
+    setLoading(false);
+  };
+  const containerProps = {
+    "aria-busy": loading,
+  };
 
   return (
     <div class={classes["number-section"]}>
@@ -61,28 +20,63 @@ function Numbers() {
       <div class={classes["notes-books-pyq"]}>
         <div>
           <span class={`${classes["notes-number"]} ${classes["number"]}`}>
-            {counter}+
+            {/* {counter}+ */}
+            <CountUp
+              class={`${classes["downloads-number"]} ${classes.number}`}
+              end={1839}
+              duration="5"
+              onStart={onStart}
+              onEnd={onEnd}
+              containerProps={containerProps}
+            />
+            +
           </span>
           <br />
           <h2 class={`${classes["notes-label"]} ${classes.label}`}>Notes</h2>
         </div>
         <div>
           <span class={`${classes["books-number"]} ${classes.number}`}>
-            {counter1}+
+            {/* {counter1}+ */}
+            <CountUp
+              class={`${classes["downloads-number"]} ${classes.number}`}
+              end={512}
+              duration="5"
+              onStart={onStart}
+              onEnd={onEnd}
+              containerProps={containerProps}
+            />
+            +
           </span>
           <br />
           <h2 class={`${classes["books-label"]} ${classes.label}`}>Books</h2>
         </div>
         <div>
           <span class={`${classes["pyqs-number"]} ${classes.number}`}>
-            {counter2}+
+            {/* {counter2}+ */}
+            <CountUp
+              class={`${classes["downloads-number"]} ${classes.number}`}
+              end={729}
+              duration="5"
+              onStart={onStart}
+              onEnd={onEnd}
+              containerProps={containerProps}
+            />
+            +
           </span>
           <br />
           <h2 class={`${classes["pyqs-label"]} ${classes.label}`}>PYQ's</h2>
         </div>
         <div>
           <span class={`${classes["downloads-number"]} ${classes.number}`}>
-            {counter3}+
+            <CountUp
+              class={`${classes["downloads-number"]} ${classes.number}`}
+              end={2729}
+              duration="5"
+              onStart={onStart}
+              onEnd={onEnd}
+              containerProps={containerProps}
+            />
+            +
           </span>
           <br />
           <h2 class={`${classes["downloads-label"]} ${classes.label}`}>
