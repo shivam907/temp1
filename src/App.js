3153d6fa7a9337db7961208fd8b881722bf8e064
shivam907 <shivamkaushal907@gmail.com>
2023-07-12 13:09:51 +0530

    fixed major bugs

--- src/App.js
+++ src/App.js
@@ -29,9 +29,24 @@ function App() {
         <Loader />
       ) : (
         <>
-          <LandingPage />
-          <SelectSemester />
-          <SubjectPage />
+          <html lang="en">
+            <head>
+              <meta charset="UTF-8" />
+              <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+              <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1.0"
+              />
+              {/* <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" /> */}
+
+              <title>Document</title>
+            </head>
+            <body>
+              <LandingPage />
+              <SelectSemester />
+              <SubjectPage />
+            </body>
+          </html>
         </>
       )}
     </>
--- src/index.js
+++ src/index.js
@@ -162,7 +162,18 @@ const router = createBrowserRouter([
 ]);
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+      </head>
+      <body>
+        <RouterProvider router={router} />
+      </body>
+    </html>
+
     {/* <App /> */}
   </React.StrictMode>
 );
