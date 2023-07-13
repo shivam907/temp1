83c4a5ba673f55955a63b18dffe2c4044a2ecd01
shivam907 <shivamkaushal907@gmail.com>
2023-07-13 20:32:42 +0530

    corrected the images

--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -12,7 +12,7 @@ function Blogs() {
       </h1>
       <div class={classes["boxes"]}>
         <BlogBox
-          img={img1}
+          img={img2}
           heading={"Five Essential Online Learning Strategies"}
           description={
             "By employing a few essential strategies, any student can learn not only to succeed, but to flourish in the online learning environment."
@@ -23,7 +23,7 @@ function Blogs() {
           }
         />
         <BlogBox
-          img={img2}
+          img={img3}
           heading={"How to Give Yourself Grace to Start Again"}
           description={
             "Here are some tips which will apply in so many different situations in life, from fitness goals to business goals to raising children."
@@ -34,7 +34,7 @@ function Blogs() {
           }
         />
         <BlogBox
-          img={img3}
+          img={img1}
           heading={"Begineer Guide To Start Competitive Programming"}
           description={
             "Here are some of the things to keep in your mind before going for a competitive programming such as choose your language and so on..."
